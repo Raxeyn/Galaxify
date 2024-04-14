361960bc173dff1b3a2abb96c800340b489611f3
Darkwill <tahaultay54@gmail.com>
2024-04-14 12:18:02 +0300

    g

---
+++ src/config.js
@@ -0,0 +1,22 @@
+export default {
+  TOKEN: "NOT HERE, IN ENV",
+  MONGO: "",
+  OWNER: ["1057674644905279498"],
+  PREFIX: ",",
+  WEBHOOK: "",
+  COLOR: "#1e5eb9",
+  EMOJIS: {
+    TICK: "",
+    ERROR: "",
+  },
+  NODES: [
+    {
+      name: "RAM",
+      url: "lava.link:80",
+      auth: "kronix",
+      secure: false,
+    },
+  ],
+  SpotifyID: '57c967ba860e43d4b8ef75a25611a5e4',
+  SpotifySecret: '3555b9cb1e4c4eb1a5e81e5babedd875',
+};
